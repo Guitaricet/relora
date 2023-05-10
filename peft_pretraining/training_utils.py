edba1a0b5e2fe525b29dec4c205fb555e5a75413
Vladislav Liain <guitaricet@gmail.com>
2023-05-10 16:11:04 -0400

    add cyclical_cosine scheduler

--- peft_pretraining/training_utils.py
+++ peft_pretraining/training_utils.py
@@ -6,7 +6,7 @@ from torch.optim.lr_scheduler import LambdaLR
 import transformers
 
 
-def get_scheculer(optimizer, scheduler_type, num_training_steps, warmup_steps, min_lr_ratio):
+def get_scheculer(optimizer, scheduler_type, num_training_steps, warmup_steps, min_lr_ratio, cycle_length=None):
     if scheduler_type == "linear":
         return transformers.get_linear_schedule_with_warmup(
             optimizer,
@@ -20,6 +20,15 @@ def get_scheculer(optimizer, scheduler_type, num_training_steps, warmup_steps, m
             num_training_steps=num_training_steps,
             min_lr_ratio=min_lr_ratio,
         )
+    if scheduler_type == "cyclical_cosine":
+        return get_cyclical_cosine_schedule_with_min_lr(
+            optimizer,
+            num_warmup_steps=warmup_steps,
+            cycle_length=cycle_length,
+            num_training_steps=num_training_steps,
+            min_lr_ratio=min_lr_ratio,
+        )
+
     raise NotImplementedError(f"Scheduler {scheduler_type} is not implemented")
 
 
@@ -32,6 +41,32 @@ def get_cosine_schedule_with_min_lr(optimizer, num_warmup_steps, num_training_st
         min_lr_ratio=min_lr_ratio,
     )
     return LambdaLR(optimizer, lr_lambda, last_epoch)
+
+
+def get_cyclical_cosine_schedule_with_min_lr(optimizer, num_warmup_steps, cycle_length, num_cycles=0.5, min_lr_ratio=0.1, last_epoch=-1):
+    lr_lambda = partial(
+        _get_cyclical_cosine_schedule_with_min_lr_lambda,
+        num_warmup_steps=num_warmup_steps,
+        cycle_length=cycle_length,
+        num_cycles=num_cycles,
+        min_lr_ratio=min_lr_ratio,
+    )
+    return LambdaLR(optimizer, lr_lambda, last_epoch)
+
+
+def _get_cyclical_cosine_schedule_with_min_lr_lambda(current_step, *, num_warmup_steps, cycle_length, num_cycles, min_lr_ratio):
+    assert 0 < min_lr_ratio <= 1.0, "min_lr_ratio must be in (0,1]"
+    
+    # compute where we are in the current cycle
+    cycle_step = current_step % cycle_length
+    
+    if cycle_step < num_warmup_steps:
+        return float(cycle_step) / float(max(1, num_warmup_steps))
+    
+    progress = float(cycle_step - num_warmup_steps) / float(max(1, cycle_length - num_warmup_steps))
+    cosine_decay = 0.5 * (1.0 + math.cos(math.pi * float(num_cycles) * 2.0 * progress))
+    
+    return min_lr_ratio + (1.0 - min_lr_ratio) * cosine_decay
 
 
 def _get_cosine_schedule_with_min_lr_lambda(current_step, *, num_warmup_steps, num_training_steps, num_cycles, min_lr_ratio):
