15cf7b1f7e883727f1ed226dc035858accbcfd10
Vladislav Liain <guitaricet@gmail.com>
2023-09-01 23:16:07 -0400

    add flash attention 2 support (optional for now)

--- torchrun_main.py
+++ torchrun_main.py
@@ -42,6 +42,7 @@ from loguru import logger
 from peft_pretraining import training_utils, args_utils
 from peft_pretraining.dataloader import SkipDataLoader
 from peft_pretraining.modeling_llama import LlamaForCausalLM
+from peft_pretraining.modeling_llama_flash2 import LlamaForCausalLM as LlamaForCausalLMFlash2
 from peft_pretraining.modeling_pythia import GPTNeoXForCausalLM
 from peft_pretraining.relora import ReLoRaModel, ReLoRaLinear, merge_and_reinit_functional
 
@@ -126,6 +127,7 @@ def parse_args(args=None):
     parser.add_argument("--profile", default=False, type=lambda x: x.lower() == "true")
     parser.add_argument("--autoresume", default=False, type=lambda x: x.lower() == "true")
     parser.add_argument("--comment", type=str, default=None, help="Wandb notes")
+    parser.add_argument("--flash_attention_2", default=False, type=lambda x: x.lower() == "true")
 
     parser.add_argument("--seed", type=int, default=0)
 
@@ -472,11 +474,15 @@ def main(args):
             else:
                 raise ValueError(f"Model config vocab size ({model_config.vocab_size}) does not match tokenizer vocab size ({t_vocab_size})")
 
-        if isinstance(model_config, LlamaConfig):
-            logger.info("Using local version of LLaMA")
-            model = LlamaForCausalLM(model_config)
-        else:
+        if not isinstance(model_config, LlamaConfig):
             raise NotImplementedError(f"Unknown model config type {type(model_config)}, only LLaMA is supported")
+
+        logger.info("Using local version of LLaMA")
+        if args.flash_attention_2:
+            logger.info("Using Flash Attention 2")
+            model = LlamaForCausalLMFlash2(model_config)
+        else:
+            model = LlamaForCausalLM(model_config)
     else:
         logger.info(f"Using HuggingFace model {args.model_name_or_path} revision {args.model_revision}")
         model = GPTNeoXForCausalLM.from_pretrained(args.model_name_or_path, revision=args.model_revision)
